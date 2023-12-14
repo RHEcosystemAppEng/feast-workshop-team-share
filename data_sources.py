14fa80363d770c03d69270aa2aaa23677330e66e
Jeremy Ary <jary@redhat.com>
2023-12-14 11:28:27 -0600

    initial commit

---
+++ data_sources.py
@@ -0,0 +1,11 @@
+from feast import FileSource
+
+driver_stats = FileSource(
+    name="driver_stats_source",
+    path="s3://feast-workshop-team-share/driver_stats.parquet",  # TODO: Replace with your bucket
+    s3_endpoint_override="http://s3.us-west-1.amazonaws.com",  # Needed since s3fs defaults to us-east-1
+    timestamp_field="event_timestamp",
+    created_timestamp_column="created",
+    description="A table describing the stats of a driver based on hourly logs",
+    owner="test2@gmail.com",
+)
